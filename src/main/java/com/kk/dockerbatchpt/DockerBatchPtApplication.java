9c0f82a8de8e1795641b3bacbc80b37c06b559a3
triptocode <3580rio@gmail.com>
2023-07-09 11:12:45 +0900

    first commit

---
+++ src/main/java/com/kk/dockerbatchpt/DockerBatchPtApplication.java
@@ -0,0 +1,13 @@
+package com.kk.dockerbatchpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerBatchPtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerBatchPtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kk/dockerbatchpt/DockerBatchPtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kk.dockerbatchpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerBatchPtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
